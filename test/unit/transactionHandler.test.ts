f30770df5c3a7fe26fef534acc128f893022305c
kopy-kat <kopp.konrad@gmail.com>
2025-04-18 10:29:12 +0200

    feat: txHandler tests

--- test/unit/transactionHandler.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from 'vitest'
-import { validateBundle } from '../../src/core/validator'
-import { getEmptyBundleEvent } from '../common/utils'
-
-describe('transactionHandler', () => {
-  it('', async () => {})
-})
