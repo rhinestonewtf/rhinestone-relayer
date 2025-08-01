c314604c8b4da858068357c5cc30312a5af4e5a1
Tadas <tadovas@gmail.com>
2025-08-01 15:29:31 +0300

    Working test

--- src/helpers/rebalancing.ts
+++ src/helpers/rebalancing.ts
@@ -1,11 +1,25 @@
-import { Address, Hex } from "viem"
+import { Address, decodeFunctionData, Hex } from "viem"
+import { routerAbi } from "../abi/router"
 
 export type RepaymentDestination = {
     address: Address,
     chain?: number,
 }
 
+const supportedRouteCalls = ['routeClaim', 'routeFill']
+
 export function replaceRepaymentDestinations(data: Hex, destination: RepaymentDestination): Hex {
+
+    const routerCall = decodeFunctionData(
+        {
+            abi: routerAbi,
+            data
+        }
+    )
+
+    if (!supportedRouteCalls.includes(routerCall.functionName)) {
+        throw new Error(`Unsupported route function call: ${routerCall.functionName}`)
+    }
 
     return data
 }
